743e5076281ebb966f311448d45ea0bc2c73c697
Tom <tom@degrunt.nl>
2011-05-18 22:34:06 +0200

    first commit

---
+++ compile.js
@@ -0,0 +1,17 @@
+var stylus = require('stylus')
+    , fs = require('fs')
+    , str = require('fs').readFileSync(__dirname + '/aristo/index.styl', 'utf8');
+
+  stylus(str)
+    .set('filename', __dirname + '/aristo/index.styl')
+    .define('url', stylus.url({ paths: [__dirname + '/images'], limit: 50000 }))
+    .render(function(err, css){
+
+      if (err) throw err;
+
+      fs.writeFile('aristo.css', css, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+      });
+  });
+
